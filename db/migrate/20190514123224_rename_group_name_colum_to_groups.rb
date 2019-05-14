3ee43c2d30a1cc475485098f24de47cb512b0ef3
create <silence.24yue@gmail.com>
2019-05-14 21:51:22 +0900

    Add test_messages_controller_display

---
+++ db/migrate/20190514123224_rename_group_name_colum_to_groups.rb
@@ -0,0 +1,5 @@
+class RenameGroupNameColumToGroups < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :groups, :group_name, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190513064801) do
+ActiveRecord::Schema.define(version: 20190514123224) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20190513064801) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "group_name", null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
