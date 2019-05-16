4762c781fc8daf8ca7424cdeb7cbc556bebb6071
create <silence.24yue@gmail.com>
2019-05-16 12:51:37 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){
   function buildHTML(message){
-    var MessageImage = ``;
-    MessageImage = (message.image) ? `<img src="${ message.image }">`: "";
+    var　MessageImage = (message.image) ? `<img src="${ message.image }">`: "";
 
     var html = `<div class="message" data-message-id="${message.id}">
                   <div class="upper-info">
